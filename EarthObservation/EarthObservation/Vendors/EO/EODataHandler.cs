b9a5d0fda9594f7b299dc2ab599de09109b00276
StephenGoedhart <sdg25@hotmail.com>
2019-03-28 17:08:53 +0100

    EO

--- EarthObservation/EarthObservation/Vendors/EO/EODataHandler.cs
+++ EarthObservation/EarthObservation/Vendors/EO/EODataHandler.cs
@@ -14,19 +14,23 @@ namespace EarthObservation.Vendors.EO
     class EODataHandler
     {
         static Thread background = new Thread(new ThreadStart(GetLatest));
-        static bool busy;
+        static bool busy = false;
         /// <summary>
         /// Start gathering satellite imagery on a new thread. 
         /// </summary>
         public static void Start()
         {
-            busy = true;
-            background.IsBackground = true;
-            background.Start();
+            if (!busy)
+            {
+                busy = true;
+                background.IsBackground = true;
+                background.Start();
+            }
         }
         public static void Stop()
         {
-            background.Abort();
+            if(!busy)
+                background.Abort();
         }
 
         private static void GetLatest()
@@ -34,7 +38,9 @@ namespace EarthObservation.Vendors.EO
             var url = "https://eumetview.eumetsat.int/geoserv/wms?SERVICE=WMS&REQUEST=GetMap&TRANSPARENT=TRUE&EXCEPTIONS=INIMAGE&VERSION=1.3.0&LAYERS=meteosat%3Amsg_naturalenhncd&STYLES=raster&SRS=EPSG%3A4326&WIDTH=3712&HEIGHT=3712&BBOX=-76.9170259,-77,77.0829741,77&FORMAT=image%2Fgeotiff";
             var web = new WebClient();
             var time = DateTime.Now.ToString("yyyyMMddTHHmm");
-            web.DownloadFile(url, "../../Models/EO/Eumetsat"+time+".tif");            
+            web.DownloadFile(url, "../../Models/EO/Eumetsat"+time+".tif");
+            busy = false;
+            Stop();
         }
     }
 }
