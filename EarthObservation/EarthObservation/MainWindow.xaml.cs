bf95811199545d39248fcbd404ec02dd6d579b22
StephenGoedhart <sdg25@hotmail.com>
2019-03-28 15:45:33 +0100

    Working on multithreaded gathering of satellite imagery

--- EarthObservation/EarthObservation/MainWindow.xaml.cs
+++ EarthObservation/EarthObservation/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EarthObservation.Vendors.EO;
 
 namespace EarthObservation
 {
@@ -22,6 +23,7 @@ namespace EarthObservation
     {
         public MainWindow()
         {
+            EODataHandler.Start();
             InitializeComponent();
         }
     }
--- EarthObservation/EarthObservation/Vendors/EO/EODataHandler.cs
+++ EarthObservation/EarthObservation/Vendors/EO/EODataHandler.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 
 namespace EarthObservation.Vendors.EO
 {
+    /// <summary>
+    /// Earth Observation Event Handler. Use this class to start the gathering of satellite imagery. 
+    /// </summary>
     class EODataHandler
     {
-        public static void GetLatest()
+        static Thread background = new Thread(new ThreadStart(GetLatest));
+        static bool busy;
+        /// <summary>
+        /// Start gathering satellite imagery on a new thread. 
+        /// </summary>
+        public static void Start()
+        {
+            busy = true;
+            background.IsBackground = true;
+            background.Start();
+        }
+        public static void Stop()
+        {
+            background.Abort();
+        }
+
+        private static void GetLatest()
         {
             var url = "https://eumetview.eumetsat.int/geoserv/wms?SERVICE=WMS&REQUEST=GetMap&TRANSPARENT=TRUE&EXCEPTIONS=INIMAGE&VERSION=1.3.0&LAYERS=meteosat%3Amsg_naturalenhncd&STYLES=raster&SRS=EPSG%3A4326&WIDTH=3712&HEIGHT=3712&BBOX=-76.9170259,-77,77.0829741,77&FORMAT=image%2Fgeotiff";
+            var web = new WebClient();
+            var time = DateTime.Now.ToString("yyyyMMddTHHmm");
+            web.DownloadFile(url, "../../Models/EO/Eumetsat"+time+".tif");            
         }
     }
 }
